5d53a8b042ed14401962e5afc74f4d51998ed616
Vladimir Shevchuk <dosandkv@gmail.com>
2016-02-05 17:51:20 +0200

    added task for img build;

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ var clean = require('gulp-clean');
 /*task for local server*/
 var connect = require('gulp-connect');
 
+gulp.task('build', ['build:css', 'build:img', 'build:ui-min-js']);
+
 gulp.task('clean', function () {
     return gulp.src('./dist', { read: false })
         .pipe(clean());
@@ -24,21 +26,26 @@ gulp.task('js-watch', function() {
     gulp.watch('app/js/**/*.js', ['build-ui-min-js']);
 });
 
-gulp.task('build-css', ['compile-scss'], function() {
+gulp.task('build:img', function() {
+    return gulp.src(['./app/img/**'])
+        .pipe(gulp.dest('./dist/img'));
+});
+
+gulp.task('build:css', ['compile-scss'], function() {
     gulp.src(['./app/css/**/*.css'])
         .pipe(concat('ui.min.css'))
         .pipe(minify())
         .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('build-core-min-js', function() {
+gulp.task('build:core-min-js', function() {
     return gulp.src(['./app/js/core/main-core.js'])
 
         .pipe(webpack(require('./webpack.config.js')))
         .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('build-ui-min-js', function() {
+gulp.task('build:ui-min-js', function() {
     return gulp.src(['./app/js/ui/main-ui.js'])
 
         .pipe(webpack(require('./webpack.config.js')))
@@ -54,6 +61,3 @@ gulp.task('compile-scss', function() {
         }))
         .pipe(gulp.dest('./app/css'));
 });
-
-
-gulp.task('default', ['clean', 'runLocalServer', 'build-css', 'build-core-min-js' , 'compile-scss']);
